cc4d318bee961575a3c3cd95216e83d29fd1e7c3
Rodrigo Sellanes <rs08@ucema.edu.ar>
2022-10-08 17:30:13 +0200

    add procjet and gitignore

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import mocks.mocks_manager as mock
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/purchase-orders/', methods=['GET'])
+def get_orders():
+    return mock.read_mock_json(mock.MOCK_ID_GET_ALL)
+
+
+@app.route('/api/v1/purchase-orders/<order_id>', methods=['GET'])
+def get_order_by_id(order_id):
+    print(f"Requested ID: {order_id}")
+    return mock.read_mock_json(mock.MOCK_ID_GET_ONE)
+
+
+@app.route('/api/v1/purchase-orders/', methods=['POST'])
+def create_order():
+    return mock.read_mock_json(mock.MOCK_ID_POST)
+
+
+@app.route('/api/v1/purchase-orders/<order_id>', methods=['PUT'])
+def modify_order_by_id(order_id):
+    print(f"Requested ID: {order_id}")
+    return mock.read_mock_json(mock.MOCK_ID_PUT)
+
+
+if __name__ == '__main__':
+    app.run()
