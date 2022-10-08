52e8a0d84d9c905c89b4637407072ad98083edec
Rodrigo Sellanes <rs08@ucema.edu.ar>
2022-10-08 18:34:46 +0200

    add cors

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 import mocks.mocks_manager as mock
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/api/v1/purchase-orders/', methods=['GET'])
