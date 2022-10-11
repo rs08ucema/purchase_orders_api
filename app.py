aed3d111ba9c41e52a0d641dbc82e571f44fac33
Rodrigo Sellanes <rs08@ucema.edu.ar>
2022-10-11 19:53:56 +0200

    add new mock examples

--- app.py
+++ app.py
@@ -11,20 +11,20 @@ def get_orders():
     return mock.read_mock_json(mock.MOCK_ID_GET_ALL)
 
 
-@app.route('/api/v1/purchase-orders/<order_id>', methods=['GET'])
-def get_order_by_id(order_id):
-    print(f"Requested ID: {order_id}")
-    return mock.read_mock_json(mock.MOCK_ID_GET_ONE)
-
-
 @app.route('/api/v1/purchase-orders/', methods=['POST'])
 def create_order():
     return mock.read_mock_json(mock.MOCK_ID_POST)
 
 
+@app.route('/api/v1/purchase-orders/<order_id>', methods=['GET'])
+def get_order_by_id(order_id):
+    print(f"GET - The user requested order # {order_id}")
+    return mock.read_mock_json(mock.MOCK_ID_GET_ONE)
+
+
 @app.route('/api/v1/purchase-orders/<order_id>', methods=['PUT'])
-def modify_order_by_id(order_id):
-    print(f"Requested ID: {order_id}")
+def edit_order_by_id(order_id):
+    print(f"PUT - The user requested order # {order_id}")
     return mock.read_mock_json(mock.MOCK_ID_PUT)
 
 
